87f0e0e3229509bc669036f1d8234598040f6ff6
acanon74 <69167857+Pixel74@users.noreply.github.com>
2022-12-09 00:09:17 +0500

    Added ex 1.12-1.29

---
+++ SICP 1.29/SICP 1.29.rkt
@@ -0,0 +1,35 @@
+
+#lang racket/base
+
+
+
+
+;;(define h (/ (- b a) n))
+
+(define (s a b n)
+
+
+  (define (cube x) (* x x x))
+  (define f cube)
+
+  (define h (/ (- b a) n))
+
+  (define (term x)
+    (+ (f x) (* 2 (f (+ x h))) (* 4 (f (+ x (* 2 h))))
+    )
+  )
+
+  (define (next x)
+    (+ x (* 2 h))
+  )
+
+
+  (define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+  (* (/ h 3) (sum term a next b))
+
+)
